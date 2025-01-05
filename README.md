d9896d759a330dc614c0b9830ce581df5c4ca19c
Ryan Christian <rchristian@ryanchristian.dev>
2025-01-05 03:27:51 -0600

    feat: Initializing repository

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">vim-markdown-preview</h1>
+
+<div align="center">
+    <a href="https://github.com/rschristian/vim-markdown-preview/blob/master/LICENSE">
+        <img
+            alt="NPM"
+            src="https://img.shields.io/badge/license-MIT-brightgreen"
+        />
+    </a>
+</div>
+
+<br />
+
+Provides a quick and light method of previewing markdown files locally in your browser.
+
+> **Important**: Still under development! Lot of styling work to do yet.
+
+## Installation
+
+> Make sure you have node support for neovim.
+> Run the command `npm install -g neovim` to add it.
+
+Use your favorite plugin manager to install the plugin.
+I use [vim-plug](https://github.com/junegunn/vim-plug).
+
+```vim
+Plug('rschristian/vim-markdown-preview', {['do'] = 'npm install --omit=dev'})
+```
+
+After installing the plugin, run `:UpdateRemotePlugins` to register it with Neovim.
+
+## Usage
+
+To preview a markdown file, run the command `:MarkdownPreview` while in normal mode.
+
+## Alternatives
+
+Other options for you to checkout if this does not work for you:
+
+- [iamcco/markdown-preview.nvim](https://github.com/iamcco/markdown-preview.nvim)
+
+## License
+
+MIT © Ryan Christian
---
+++ rplugin/node/vim-markdown-preview/index.js
@@ -0,0 +1,50 @@
+import os from 'node:os';
+import path from 'node:path';
+import { promises as fs } from 'node:fs';
+import childProcess from 'node:child_process';
+
+import { marked } from 'marked';
+import Prism from 'prismjs';
+import loadLanguages from 'prismjs/components/index.js';
+
+import { templateHtml } from './templates.js';
+
+marked.use({
+    renderer: {
+        code({ text, lang }) {
+            if (!lang) return `<pre><code>${text}</code></pre>`;
+            if (!Prism.languages[lang]) loadLanguages([lang]);
+
+            const highlight = Prism.highlight(text, Prism.languages[lang], lang);
+            return `<pre class="highlight"><code class="language-${lang}">${highlight}</code></pre>`;
+        },
+    },
+});
+
+/**
+ * @param {import('neovim').NvimPlugin} plugin
+ */
+async function run(plugin) {
+    const buffer = await plugin.nvim.buffer
+    const bufferLines = await buffer.getLines();
+    const bufferContent = bufferLines.join('\n');
+
+    const html = await marked(bufferContent);
+
+    const tempFile = path.join(os.tmpdir(), 'rschristian--vim-markdown-preview.html');
+    await fs.writeFile(tempFile, templateHtml(html));
+
+    return childProcess.spawn('xdg-open', [tempFile]);
+}
+
+/**
+ * @param {import('neovim').NvimPlugin} plugin
+ */
+export default function Plugin(plugin) {
+    plugin.setOptions({ dev: false });
+
+    plugin.registerCommand(
+        'MarkdownPreview',
+        async () => await run(plugin),
+    );
+}
