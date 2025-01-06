1128360bdf8a291f9e31b643164a6c471b53e10c
Ryan Christian <rchristian@ryanchristian.dev>
2025-01-06 01:38:28 -0600

    refactor: Flesh out highlights a bit, improve loading scheme

--- rplugin/node/vim-markdown-preview/index.js
+++ rplugin/node/vim-markdown-preview/index.js
@@ -1,4 +1,5 @@
 import os from 'node:os';
+import url from 'node:url';
 import path from 'node:path';
 import { promises as fs } from 'node:fs';
 import childProcess from 'node:child_process';
@@ -7,16 +8,22 @@ import { marked } from 'marked';
 import Prism from 'prismjs';
 import loadLanguages from 'prismjs/components/index.js';
 
-import { templateHtml } from './templates.js';
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
+
+let readTemplateHtml;
 
 marked.use({
     renderer: {
         code({ text, lang }) {
-            if (!lang) return `<pre><code>${text}</code></pre>`;
-            if (!Prism.languages[lang]) loadLanguages([lang]);
+            if (lang) {
+                if (lang == 'jsonc') lang = 'json5';
+                if (lang == 'js' && text.includes('<')) lang = 'jsx';
 
-            const highlight = Prism.highlight(text, Prism.languages[lang], lang);
-            return `<pre class="highlight"><code class="language-${lang}">${highlight}</code></pre>`;
+                if (!Prism.languages[lang]) loadLanguages([lang]);
+                text = Prism.highlight(text, Prism.languages[lang], lang);
+            }
+
+            return `<pre class="highlight"><code class="${lang ? 'language-' + lang : ''}">${text}</code></pre>`;
         },
     },
 });
@@ -30,10 +37,18 @@ async function run(plugin) {
     const bufferContent = bufferLines.join('\n');
     const bufferName = await buffer.name;
 
-    const html = await marked(bufferContent);
+    if (!readTemplateHtml) {
+        readTemplateHtml = fs.readFile(path.join(__dirname, 'template.html'), 'utf-8');
+    }
+
+    const [templateHtml, htmlified] = await Promise.all([readTemplateHtml, marked(bufferContent)]);
+
+    const constructedTemplate = templateHtml
+        .replace('<% bufferName %>', bufferName)
+        .replace('<% bufferContent %>', htmlified);
 
     const tempFile = path.join(os.tmpdir(), 'rschristian--vim-markdown-preview.html');
-    await fs.writeFile(tempFile, templateHtml({ bufferName, bufferContent: html }));
+    await fs.writeFile(tempFile, constructedTemplate);
 
     return childProcess.spawn('xdg-open', [tempFile]);
 }
